b676be008260ba9f5d28ff12cce3b379801572b6
Lawrence <lawrenceevalyn@gmail.com>
2018-12-13 17:55:45 -0500

    started pseudocode for reprint-finder

---
+++ programs/reprint-finder/finder.py
@@ -0,0 +1,7 @@
+take in a directory and filename
+
+discard the information that I won't care about for this process
+
+for each row in the csv,
+
+it would be way too inefficient to compare it to every other row in the csv, since there are 50k.
