6f49b99da78affd82d5d4312db812802af3ad162
Lawrence <lawrenceevalyn@gmail.com>
2019-02-02 20:04:43 -0500

    able to put stuff from randomsource into outputfile!

--- programs/random-sampler/sampler.py
+++ programs/random-sampler/sampler.py
@@ -1,27 +1,54 @@
+import os
+
 # HARD-CODE THESE BEFORE YOU DO ANYTHING
 textsourcename = "testcorpus.csv" # must be a csv
 randomsourcename = "testsample.txt" # must be a txt with one number per line
 
+
 # This is the actual code that uses the info provided above
-#outputfilename = textsource + "-" + randomsource
-#outputfile = outputfilename.csv
 
-textsource = open(textsourcename, "r")
-randomsource = open(randomsourcename, "r")
+# open the source files
+# textsource = open(textsourcename, "r")
+# randomsource = open(randomsourcename, "r")
 
-print ("textsource: ")
-print (textsourcename)
-print (textsource)
+# automatically name output file based on inputs
+outputfilename = ((os.path.splitext(textsourcename)[0])) + "-" + ((os.path.splitext(randomsourcename)[0])) + ".csv"
 
-print ("randomsource: ")
-print (randomsourcename)
-print (randomsource)
+outputfile = open(outputfilename, "w") # creates blank output file
+outputfile.close()					   # important so re-running rewrites!!
 
-# print "outputfile: " + outputfile
+# make sure the files above work
+#print ("textsource: ")
+#print (textsourcename)
+#print (textsource.read())
 
-#for each line in randomsource
-#	sampleno = [the number on that line]
+#print ("randomsource: ")
+#print (randomsourcename)
+#print (randomsource.read())
+
+#print ("outputfile: ")
+#print (outputfilename)
+#print (outputfile.read())
+
+# TIME TO LOOP
+with open(randomsourcename) as fp: # can I do this in one line...?
+	for line in fp: 
+		
+		# get the next number in the list
+		sampleno = line
+		print("sampleno: ")
+		print(sampleno)
+		
+		# append that number to the output file
+		# (later change this so it appends the desired data from textsource)
+		fh = open(outputfilename, "a")
+		fh.write(sampleno) # this is the thing to change what gets appended
+		fh.close()
+
+print ("outputfile: " + outputfilename)
+fo = open(outputfilename, "r")
+print (fo.read())
+fo.close()
+
 #	get the line in textsource at sampleno
 #	append that line to outputfile
-	
-# is that all I need? this IS a really simple task...
