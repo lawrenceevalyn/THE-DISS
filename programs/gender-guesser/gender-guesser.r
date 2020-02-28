70a1613d147f924a2d3f08f9590a47818bc270d6
Lawrence <lawrenceevalyn@gmail.com>
2020-02-27 21:49:28 -0500

    got a rough sense of how to use gender-guesser.r !!

--- programs/gender-guesser/gender-guesser.r
+++ programs/gender-guesser/gender-guesser.r
@@ -3,12 +3,16 @@
 # Think of this as a place to keep scratch notes, rather than a program,
 # and copy output from the console to long-term storage.
 
-library(gender)
+library(gender) # re-run these every time to get everything necessary installed
+install_genderdata_package()
+devtools::install_github("ropensci/genderdata")
 
-data = "ECCO-1789-sample.csv"
+names = c("john", "madison")
 
-# gender_df(data, name_col = "Author", year_col = "Standardized Year",
-#           method = c("napp"))
+ECCOnames = c("Charles", "Society", "William", "René-Louis", "James", "John", "Edward", "Philip", "Hainault", "Junius", "Septimus", "Herodian", "John", "Benjamin", "Samuel", "Whitwell", "William", "Augustus", "Francis", "William", "Olaudah", "Candid", "John", "Charles", "William", "John", "Great", "Andrew", "Charles", "John", "Society", "John", "Western", "Elhanan", "John", "William", "Joshua", "Ann", "Charlotte")
 
+gender(ECCOnames, method = "napp", years = 1789)
+# if this makes an empty tibble, it's probably fine, but I'm asking for something it has no data for (probably because the date is too early)
+# ipums can go as early as 1789
+# napp can go as early as 1758, but doesn't know the name "John" until 1769
 
-gender("madison", method = "demo", years = 1985)
