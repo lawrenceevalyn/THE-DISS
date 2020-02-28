26f284ac5621261ad1f3cbbaf44fd904fd043371
Lawrence <lawrenceevalyn@gmail.com>
2020-02-27 20:40:34 -0500

    wrote a bunch of stuff on data structures

--- programs/gender-guesser/gender-guesser.r
+++ programs/gender-guesser/gender-guesser.r
@@ -1,6 +1,11 @@
+# Reminder to self: the way R works is that you essentually run the code yourself,
+# line by line, rather than following the python workflow of running whole "programs".
+# Think of this as a place to keep scratch notes, rather than a program,
+# and copy output from the console to long-term storage.
+
 library(gender)
 
-# data = "ECCO-1789-sample.csv"
+data = "ECCO-1789-sample.csv"
 
 # gender_df(data, name_col = "Author", year_col = "Standardized Year",
 #           method = c("napp"))
